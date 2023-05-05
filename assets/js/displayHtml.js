19412cedf232078efe07e5295ea64ded1186dc47
luciano <luciano.cia3@gmai.com>
2023-05-05 14:03:49 +0200

    Style des Todo

--- assets/js/displayHtml.js
+++ assets/js/displayHtml.js
@@ -10,10 +10,17 @@ export function displayHtml(listToDo) {
     divImgTodo.className = `articleTodo__divImgTodo ${toDo.img}`;
     divContentTodo.className = "articleTodo__divContentTodo";
     divFuncTodo.className = "articleTodo__divFuncTodo";
-    divContentTodo.innerHTML = `<h4 class="divContentTodo__title">${toDo.title}</h4>
+    divContentTodo.innerHTML = `<h4 class="divContentTodo__title">${
+      toDo.title
+    }</h4>
     <span class="divContentTodo__contentTxt">${toDo.contentTxt}</span>  
-    <input type="datetime-local" class="divContentTodo__date" value="${toDo.date}">
-    `;
+    <input type="datetime-local" class="divContentTodo__date" value="${
+      toDo.date
+    }">
+    <span class="divContentTodo__States ${toDo.states
+      .toLowerCase()
+      .replace(" ", "")}"> ${toDo.states}</span>`;
+    divFuncTodo.innerHTML = `<div>TEST TEST</div>`;
 
     articleToDo.appendChild(divImgTodo);
     articleToDo.appendChild(divContentTodo);
